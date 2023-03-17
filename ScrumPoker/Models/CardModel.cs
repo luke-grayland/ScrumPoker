d8400a78d50548abbee5cd7295d0de2748f4418f
Luke Grayland <lgrayland96@gmail.com>
2023-03-17 21:56:40 +0000

    mocks created

---
+++ ScrumPoker/Models/CardModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ScrumPoker.Models
+{
+    public class CardModel
+    {
+        public int Value { get; set; }
+
+        public CardModel(int value)
+        {
+            Value = value;
+        }
+    }
+}
+
