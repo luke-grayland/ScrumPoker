658e7a5bcf1b43a423c378d6730b4306cb049288
Luke Grayland <lgrayland96@gmail.com>
2023-03-31 21:28:34 +0100

    GameModel singleton added

---
+++ ScrumPoker/Singletons/GameModelSingleton.cs
@@ -0,0 +1,25 @@
+using ScrumPoker.Models;
+
+public class GameModelSingleton
+{
+    private static GameModelSingleton _instance;
+    public GameModel Game { get; set; }
+
+    private GameModelSingleton()
+    {
+        Game = new GameModel();
+    }
+
+    public static GameModelSingleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameModelSingleton();
+            }
+            return _instance;
+        }
+    }
+
+}
